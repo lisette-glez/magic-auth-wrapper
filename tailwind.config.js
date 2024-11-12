ab7ece0e1f8e23ae63bd1044faa4e7a681f9ea0d
Lisette Gonzalez <contact@lisette.dev>
2024-11-12 11:12:48 -0500

    add tailwind integration

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./src/views/**/*.{html,ejs}", // Tailwind will look for classes in these files
+    "./public/**/*.{html,ejs}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
